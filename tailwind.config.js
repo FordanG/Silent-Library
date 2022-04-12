b355683b32c5d4ad48a089d84bc1112d17604745
FordanG <60175691+FordanG@users.noreply.github.com>
2022-04-12 21:09:11 +0800

    update homepage and books page

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,28 @@ module.exports = {
         secondary: "#F9537F",
         tertiary: "#DEF2FF",
       },
+
+      scale: {
+        102: "1.02",
+      },
+    },
+  },
+  variants: {
+    extend: {
+      display: ["hover", "group-hover"],
+      scale: ["hover", "group-hover"],
+      filter: ["group-hover"],
+      height: ["group-hover"],
+      brightness: ["group-hover"],
+      invert: ["group-hover"],
+      margin: ["group-hover", "first", "last"],
+      textAlign: ["group-hover"],
+      fontSize: ["group-hover"],
+      justifyContent: ["hover"],
+      alignItems: ["hover"],
+      backgroundColor: ["active", "focus"],
+      pointerEvents: ["hover"],
+      borderWidth: ["responsive", "first", "last", "hover", "focus"],
     },
   },
   plugins: [require("tailwindcss"), require("autoprefixer")],
